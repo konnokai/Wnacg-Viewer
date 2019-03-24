9f90c659e31499f51cd4264f51b2160b9e0b1bae
jun112561 <f20001012@Gmail.com>
2019-03-25 01:42:53 +0800

    Add files via upload

--- Wnacg閱讀器/Properties/Settings.Designer.cs
+++ Wnacg閱讀器/Properties/Settings.Designer.cs
@@ -34,5 +34,17 @@ namespace Wnacg閱讀器.Properties {
                 this["DownloadPath"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool UriSchemeInstallErrorShowMessageBox {
+            get {
+                return ((bool)(this["UriSchemeInstallErrorShowMessageBox"]));
+            }
+            set {
+                this["UriSchemeInstallErrorShowMessageBox"] = value;
+            }
+        }
     }
 }
